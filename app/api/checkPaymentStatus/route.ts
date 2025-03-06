bc9d4063ddaa016a238aff1bcd7b67885a0e2835
Paulos19 <paulohenrique.012araujo@gmail.com>
2025-03-06 15:56:42 -0300

    first commit27

--- app/api/checkPaymentStatus/route.ts
+++ app/api/checkPaymentStatus/route.ts
@@ -1,11 +1,34 @@
-// app/api/checkPaymentStatus/route.ts
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
+import Cors from "cors";
 
+// Inicializando o Prisma Client
 const prisma = new PrismaClient();
+
+// Inicializando o middleware CORS
+const cors = Cors({
+  methods: ["GET", "POST"],
+  origin: "http://localhost:3000", // Altere para o domínio de onde sua aplicação frontend será executada
+});
+
+function runMiddleware(req: NextRequest, res: any, fn: Function) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
 
 export async function POST(req: NextRequest) {
   try {
+    const res = NextResponse.next();  // Cria uma instância do NextResponse
+
+    // Executar o middleware CORS antes de processar a requisição
+    await runMiddleware(req, res, cors);
+
     const body = await req.json();
     const { txid } = body;
 
