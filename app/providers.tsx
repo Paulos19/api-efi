3ba0aa4d2a72c53357373822da35667dfadafd24
Paulos19 <paulohenrique.012araujo@gmail.com>
2025-04-30 19:15:46 -0300

    api-sqlite

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,14 +4,17 @@ import { configureAbly } from '@ably-labs/react-hooks';
 import * as Ably from 'ably';
 import { AblyProvider } from 'ably/react';
 
+const ablyKey = process.env.NEXT_PUBLIC_ABLY_API_KEY!;
+const ablyClientId = process.env.NEXT_PUBLIC_ABLY_CLIENT_ID ?? null;
+
 configureAbly({
-  key: process.env.NEXT_PUBLIC_ABLY_API_KEY!,
-  clientId: process.env.NEXT_PUBLIC_ABLY_CLIENT_ID!,
+  key: ablyKey,
+  clientId: ablyClientId as string,
 });
 
 const client = new Ably.Realtime({
-  key: process.env.NEXT_PUBLIC_ABLY_API_KEY!,
-  clientId: process.env.NEXT_PUBLIC_ABLY_CLIENT_ID!,
+  key: ablyKey!,
+  clientId: ablyClientId!,
 });
 
 export const AblyProviders = ({ children }: { children: React.ReactNode }) => (
